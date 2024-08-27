b2480c0f69059961856bc261117852a2f0d9f268
Equipo Piso 8 Sistemas <equipopiso8sistemas@javeriana.edu.co>
2024-08-27 09:49:45 -0500

    Feat: Copy update script

--- copy-certs.sh
+++ copy-certs.sh
@@ -2,7 +2,12 @@
 
 scp ca.crt sistemas@kworker3.local:/home/sistemas
 
+scp update-certs.sh sistemas@kworker3.local:/home/sistemas
+
 scp ca.crt sistemas@kworker4.local:/home/sistemas
+
+scp update-certs.sh sistemas@kworker4.local:/home/sistemas
 
 scp ca.crt sistemas@kworker5.local:/home/sistemas
 
+scp update-certs.sh sistemas@kworker5.local:/home/sistemas
