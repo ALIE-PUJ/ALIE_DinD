e0270d2b5ffc2c3bcb663d2a9ca8c909c1bea3fb
Equipo Piso 8 Sistemas <equipopiso8sistemas@javeriana.edu.co>
2024-08-27 10:13:33 -0500

    Feat: Dont require password

--- copy-certs.sh
+++ copy-certs.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-scp ca.crt sistemas@kworker3.local:/home/sistemas
-
-scp update-certs.sh sistemas@kworker3.local:/home/sistemas
-
-scp ca.crt sistemas@kworker4.local:/home/sistemas
-
-scp update-certs.sh sistemas@kworker4.local:/home/sistemas
-
-scp ca.crt sistemas@kworker5.local:/home/sistemas
-
-scp update-certs.sh sistemas@kworker5.local:/home/sistemas
+sshpass -p 'sistemas' scp ca.crt sistemas@kworker3.local:/home/sistemas
+sshpass -p 'sistemas' scp update-certs.sh sistemas@kworker3.local:/home/sistemas
+sshpass -p 'sistemas' scp ca.crt sistemas@kworker4.local:/home/sistemas
+sshpass -p 'sistemas' scp update-certs.sh sistemas@kworker4.local:/home/sistemas
+sshpass -p 'sistemas' scp ca.crt sistemas@kworker5.local:/home/sistemas
+sshpass -p 'sistemas' scp update-certs.sh sistemas@kworker5.local:/home/sistemas
